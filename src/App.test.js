cc88b19470e14f1f592b5abe122560e874f3b45b
yangsungsim <yangsungsim227@gmail.com>
2022-03-20 15:01:38 +0900

    first message

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react6161651/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/react6161651/i);
   expect(linkElement).toBeInTheDocument();
 });
